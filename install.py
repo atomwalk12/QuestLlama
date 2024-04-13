a0fd2043442f72824987a46096b7b85ab4577c05
Razvan <atomwalk12@gmail.com>
2024-04-13 14:39:38 +0200

    Complete tests regarding synthetic RAG evaluation

--- install.py
+++ install.py
@@ -10,6 +10,7 @@ os.environ['GGML_CUDA_NO_PINNED'] = "1" # Required for CUDA support
 # By default, we install the llama-cpp-python package with CUDA support.
 # subprocess.run(["pip", "install", "llama-cpp-python"], check=True)
 subprocess.run(["pip", "install", "--upgrade", "--force-reinstall", "llama-cpp-python", "--no-cache-dir"], check=True)
+# subprocess.run(["pip", "install", "llama-cpp-python[server]"])
 
 # Construct the path to the requirements.txt file in the parent directory
 script_dir = os.path.dirname(os.path.realpath(__file__))
